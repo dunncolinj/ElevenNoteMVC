07ad35731c1e79d76c2b53231df0f813170e92e2
Colin Dunn <dunncolinj@gmail.com>
2022-01-13 21:03:55 -0500

    5.05 navbar addition

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least two characters.")]
+        [MaxLength(100, ErrorMessage = "Please enter 100 characters or less.")]
+        public string Title { get; set; }
+        [MaxLength(8000)]
+        public string Content { get; set; }
+    }
+}
