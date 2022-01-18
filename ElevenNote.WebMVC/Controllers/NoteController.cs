43fff8b62e64544e4031399cc1a9aecafc2eedeb
Colin Dunn <dunncolinj@gmail.com>
2022-01-18 18:00:03 -0500

    20220118 18:00 - 7.02

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -37,10 +37,22 @@ namespace ElevenNote.WebMVC.Controllers
                 return View(model);
             }
 
+            var service = CreateNoteService();
+            if (service.CreateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Note could not be created.");
+            return View(model);
+        }
+
+        private NoteService CreateNoteService()
+        {
             var userId = Guid.Parse(User.Identity.GetUserId());
             var service = new NoteService(userId);
-            service.CreateNote(model);
-            return RedirectToAction("Index");
+            return service;
         }
     }
 }
